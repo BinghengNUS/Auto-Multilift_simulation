3967c348f8687cd1a61963255cb965fe69facb02
Yichao Gao <yichao_gao@u.nus.edu>
2025-09-27 11:52:44 +0800

    Update maintainer

--- src/px4-offboard/setup.py
+++ src/px4-offboard/setup.py
@@ -21,7 +21,7 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='jaeyoung',
-    maintainer_email='jalim@ethz.ch',
+    maintainer_email='yichao_gao@nus.edu.sg',
     description='TODO: Package description',
     license='TODO: License declaration',
     # tests_require=['pytest'],
--- src/sitl_sim/setup.py
+++ src/sitl_sim/setup.py
@@ -14,7 +14,7 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='carlson',
-    maintainer_email='1444015757@qq.com',
+    maintainer_email='yichao_gao@u.nus.edu',
     description='TODO: Package description',
     license='TODO: License declaration',
     # tests_require=['pytest'],
